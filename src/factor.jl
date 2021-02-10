aa8ddd609c93c19eee9bf0ca12b16ebb5315cf1a
kdomino <kdomino@iitis.pl>
2021-02-10 12:01:49 +0100

    Kd make solver (#59)
    * cleaning up
    
    * Custer not  defined
    
    * all tests pass
    
    * remove double tested rank_reveal
    
    * Test custom settings, indexing, etc.
    
    * improve error msg in NetworkGraph
    
    * add MPS from row
    
    * add configurations' energy
    
    * make_lower_MPS not working yet
    
    * correction
    
    * modify MPS
    
    * gnój
    
    * improve MPS for peps
    
    * add lower mps
    
    * add minor improvments
    
    * rm energies
    
    * change tests in ising
    
    * modify ising tests
    
    * improve tests
    
    * add some remarks
    
    * add boundaryMPS
    
    * replace MPS with  boundaryMPS
    
    * export boundaryMPS
    
    * start implementing peps
    
    * fix small bughs
    
    * start writing down conditional probability
    
    * some corrections
    
    * correction
    
    * another correction
    
    * change order MPS
    
    * correction
    
    * implementation of the probability computation, not tested yet
    
    * correction
    
    * some tests on pepsrow
    
    * add contract
    
    * fix a small
    
    * use nothing in contract
    
    * correction
    
    * fix a bug in contract
    
    * merge
    
    * add contract.jl and fix bug in contract function
    
    * fix a critical bug in MPO
    
    * add use case for Krzysiek
    
    * improve use case for Krzysiek
    
    * new testing file
    
    * update contract.jl slightly
    
    * update contract
    
    * update Krzysiek's example
    
    * modify krzysiek's example
    
    * add simpler example to testing_probabilities.jl
    
    * correction
    
    * split test files
    
    * correction
    
    * correction
    
    * cleaning up
    
    * simplifications and new functions to contract peps
    
    * all test but Krzysieks pass
    
    * start coding conditional_pdo
    
    * testing Krzysiek's examples - no luck
    
    * problematic tests
    
    * a slight correction
    
    * solved problem with probabilities
    
    * cleare old implementation
    
    * correction
    
    * some corrections
    
    * correction
    
    * correction
    
    * correction
    
    * a correction
    
    * corrections in benchmarks
    
    * correction in solve
    
    * correction
    
    * correction
    
    * correction
    
    * corrections in benchmarks
    
    * add type
    
    * correct line
    
    * correction
    
    * correction
    
    Co-authored-by: annamariadziubyna <73058800+annamariadziubyna@users.noreply.github.com>
    Co-authored-by: Bartłomiej Gardas <bartek.gardas@gmail.com>

---
+++ src/factor.jl
@@ -0,0 +1,95 @@
+export factor_graph
+export rank_reveal, projectors
+
+function _max_cell_num(ig::MetaGraph)
+    L = 0
+    for v ∈ vertices(ig)
+        L = max(L, get_prop(ig, v, :cell))
+    end
+    L
+end
+
+function factor_graph(
+    ig::MetaGraph,
+    num_states_cl::Int;
+    energy::Function=energy, 
+    spectrum::Function=full_spectrum
+    )
+    d = _max_cell_num(ig)
+    ns = Dict(enumerate(fill(num_states_cl)))
+    factor_graph(
+        ig,
+        ns,
+        energy=energy,
+        spectrum=spectrum
+    )
+end
+
+function factor_graph(
+    ig::MetaGraph,
+    num_states_cl::Dict{Int, Int}=Dict{Int, Int}();
+    energy::Function=energy, 
+    spectrum::Function=full_spectrum
+) 
+    L = _max_cell_num(ig)
+    fg = MetaDiGraph(L, 0.0)
+
+    for v ∈ vertices(fg)
+        cl = Cluster(ig, v)
+        set_prop!(fg, v, :cluster, cl)
+        r = prod(cl.rank)
+        num_states = get(num_states_cl, v, r)
+        sp = spectrum(cl, num_states=num_states)
+        set_prop!(fg, v, :spectrum, sp)
+        set_prop!(fg, v, :loc_en, vec(sp.energies))
+    end
+
+    for i ∈ 1:L, j ∈ i+1:L
+        v = get_prop(fg, i, :cluster)
+        w = get_prop(fg, j, :cluster)
+
+        edg = Edge(ig, v, w)
+        if !isempty(edg.edges)
+            e = SimpleEdge(i, j)
+
+            add_edge!(fg, e)
+            set_prop!(fg, e, :edge, edg)
+
+            pl, en = rank_reveal(energy(fg, edg), :PE)
+            en, pr = rank_reveal(en, :EP)
+
+            set_prop!(fg, e, :split, (pl, en, pr))
+        end
+    end
+    fg
+end
+
+function projectors(fg::MetaDiGraph, i::Int, j::Int)
+    if has_edge(fg, i, j)
+        p1, en, p2 = get_prop(fg, i, j, :split)
+    elseif has_edge(fg, j, i)
+        p2, en, p1 = get_prop(fg, j, i, :split)
+    else
+        p1 = en = p2 = ones(1, 1)
+    end
+    p1, en, p2
+end
+
+function rank_reveal(energy, order=:PE)
+    @assert order ∈ (:PE, :EP)
+    dim = order == :PE ? 1 : 2
+    
+    E, idx = unique_dims(energy, dim)
+
+    if order == :PE 
+        P = zeros(size(energy, 1), size(E, 1)) 
+    else
+        P = zeros(size(E, 2), size(energy, 2))
+    end
+
+    for (i, elements) ∈ enumerate(eachslice(P, dims=dim))
+        elements[idx[i]] = 1
+    end
+
+    order == :PE ? (P, E) : (E, P)
+end
