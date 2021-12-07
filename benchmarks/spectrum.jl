668b4c9f621fed151e26593e068b3924ab444e9b
bartekGardas <bartek.gardas@gmail.com>
2021-12-07 19:02:56 +0100

    clean up benchmarks

--- benchmarks/spectrum.jl
+++ benchmarks/spectrum.jl
@@ -1,17 +1,12 @@
 
 using SpinGlassNetworks
 
-function bench(instance::String, max_states::Int=100)
-    m = 2
-    n = 2
-    t = 24
-
-    println("Threads: ", Threads.nthreads())
-
+function bench(instance::String, size::NTuple{3, Int}, max_states::Int=100)
     ig = ising_graph(instance)
-    cl = split_into_clusters(ig, super_square_lattice((m, n, t)))
+    cl = split_into_clusters(ig, super_square_lattice(size))
     @time sp = brute_force(cl[1, 1], num_states=max_states)
     return
 end
 
-bench("$(@__DIR__)/pegasus_droplets/2_2_3_00.txt")
+println("Threads: ", Threads.nthreads())
+bench("$(@__DIR__)/pegasus_droplets/2_2_3_00.txt", (2, 2, 24))
