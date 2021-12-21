b3ecd0b3254ef3c042446955af5f58f9ae4adba2
Marek Rams <marek.rams@uj.edu.pl>
2021-12-21 20:54:24 +0100

    new indexing of pegasus lattice

--- src/lattice.jl
+++ src/lattice.jl
@@ -11,19 +11,37 @@ function super_square_lattice(size::NTuple{3, Int})
     super_square_lattice((m, 1, n, 1, t))
 end
 
+# function pegasus_lattice(size::NTuple{2, Int})
+#     m, n = size
+#     old = LinearIndices((1:24, 1:n, 1:m))
+#     map = Dict(old[k, j, i] => (i, 2*j-1) for i=1:m, j=1:n, k ∈ vcat(1:4, 9:12, 17:20))
+#     for i=1:m, j=1:n, k ∈ vcat(5:8, 13:16, 21:24) push!(map, old[k, j, i] => (i, 2*j)) end
+#     map
+# end
+
+# function pegasus_lattice(size::NTuple{3, Int})
+#     m, n, t = size  # t is number of chimera units
+#     old = LinearIndices((1:8*t, 1:n, 1:m))
+#     map = Dict(old[k, j, i] => (i, 2*j-1) for i=1:m, j=1:n, k ∈ (n * 8 + m for n ∈ 0:t-1, m ∈ 1:4))
+#     for i=1:m, j=1:n, k ∈ (n * 8 + m for n ∈ 0:t-1, m ∈ 5:8) push!(map, old[k, j, i] => (i, 2*j)) end
+#     map
+# end
+
+
 function pegasus_lattice(size::NTuple{2, Int})
     m, n = size
     old = LinearIndices((1:24, 1:n, 1:m))
-    map = Dict(old[k, j, i] => (i, 2*j-1) for i=1:m, j=1:n, k ∈ vcat(1:4, 9:12, 17:20))
-    for i=1:m, j=1:n, k ∈ vcat(5:8, 13:16, 21:24) push!(map, old[k, j, i] => (i, 2*j)) end
+    map = Dict(old[k, j, i] => (i, j, 1) for i=1:m, j=1:n, k ∈ vcat(1:4, 9:12, 17:20))
+    for i=1:m, j=1:n, k ∈ vcat(5:8, 13:16, 21:24) push!(map, old[k, j, i] => (i, j, 2)) end
     map
 end
 
 function pegasus_lattice(size::NTuple{3, Int})
     m, n, t = size  # t is number of chimera units
     old = LinearIndices((1:8*t, 1:n, 1:m))
-    map = Dict(old[k, j, i] => (i, 2*j-1) for i=1:m, j=1:n, k ∈ (n * 8 + m for n ∈ 0:t-1, m ∈ 1:4))
-    for i=1:m, j=1:n, k ∈ (n * 8 + m for n ∈ 0:t-1, m ∈ 5:8) push!(map, old[k, j, i] => (i, 2*j)) end
+    map = Dict(old[k, j, i] => (i, j, 1) for i=1:m, j=1:n, k ∈ (n * 8 + m for n ∈ 0:t-1, m ∈ 1:4))
+    for i=1:m, j=1:n, k ∈ (n * 8 + m for n ∈ 0:t-1, m ∈ 5:8) push!(map, old[k, j, i] => (i, j, 2)) end
     map
 end
+
 
