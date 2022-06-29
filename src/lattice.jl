1e93bdc29d5c99ed97837f99c15aaef330fa7ff9
annamariadziubyna <anna.maria.dziubyna@doctoral.uj.edu.pl>
2022-06-29 15:01:17 +0200

    add pegasus_lattice_masoud

--- src/lattice.jl
+++ src/lattice.jl
@@ -1,8 +1,8 @@
 export
     super_square_lattice,
     pegasus_lattice,
-    pegasus_lattice_flip12
-
+    pegasus_lattice_masoud
+    
 "Variable number of Ising graph -> Factor graph coordinate system"
 function super_square_lattice(size::NTuple{5, Int})
     m, um, n, un, t = size
@@ -25,6 +25,18 @@ function pegasus_lattice(size::NTuple{3, Int})
     )
     for i=1:m, j=1:n, k ∈ (p * 8 + q for p ∈ 0 : t-1, q ∈ 5:8)
         push!(map, old[k, j, i] => (i, j, 2))
+    end
+    map
+end
+
+function pegasus_lattice_masoud(size::NTuple{3, Int})
+    m, n, t = size  # t is number of chimera units
+    old = LinearIndices((1:8*t, 1:n, 1:m))
+    map = Dict(
+        old[k, j, i] => (i, j, 2) for i=1:m, j=1:n, k ∈ (p * 8 + q for p ∈ 0 : t-1, q ∈ 1:4)
+    )
+    for i=1:m, j=1:n, k ∈ (p * 8 + q for p ∈ 0 : t-1, q ∈ 5:8)
+        push!(map, old[k, j, i] => (i, j, 1))
     end
     map
 end
