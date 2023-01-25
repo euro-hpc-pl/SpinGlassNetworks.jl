2893fa4e92e892795f27eec2c07420ce4ce124a4
Bartek Gardas <bartek.gardas@gmail.com>
2023-01-25 20:46:48 +0100

    add comments

--- src/idea_projectors.jl
+++ src/idea_projectors.jl
@@ -16,8 +16,31 @@ Base.eltype(lp::PoolOfProjectors{T}) where T = T
 Base.length(lp::PoolOfProjectors, index::Int) = length(lp.data[lp.default_device][index])
 Base.empty(lp::PoolOfProjectors, device::Symbol) = empty!(lp.data[device])
 
+# TODO This is version for only one GPU
 function get_projector!(lp::PoolOfProjectors, index::Int, device::Symbol)
-
-    # sprawdz czy index in lp.data[device]; jesli nie to go dodaj biorac dane z lp.data[default_device]
-    lp.data[device][index]  #  dodac element do dataGPU jak jest na cpu, ale nie na gpu ???
+    if device ∉ lp.data
+        push!(lp.data[device], index => CuArray(lp.data[default_device][index]))
+    end
+    lp.data[device][index]
 end
+
+#=
+buf_a = Mem.alloc(Mem.Unified, sizeof(a))
+d_a = unsafe_wrap(CuArray{Float32,3}, convert(CuPtr{Float32}, buf_a), dims)
+finalizer(d_a) do _
+    Mem.free(buf_a)
+end
+copyto!(d_a, a)
+=#
+
+#=
+function add_projector!(lp::PoolOfProjectors, p::Proj)
+    if p in values(lp.data)
+        key = key of p_in_lp.data
+    else
+        key = generate_new_key
+        push!(lp.data, key => p)
+    end
+    key
+end
+=#
